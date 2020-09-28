28c206872258cfaf21c45e65c61ed6a0ce7ce8f2
Milan Winter <milanwinter8@gmail.com>
2020-09-28 23:01:56 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -2,4 +2,10 @@
 def run_guessing_game
   number = rand(6) +1
   guess = gets.chomp
+  if guess == number
+    puts "You gueesed the correct number!"
+  elsif guess != number
+    puts "Sorry! The computer guessed 6."
+    
+  end
 end
