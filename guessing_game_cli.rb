94f4fb8a6621adf75d4e6ebb75de7a7ad654b274
Milan Winter <milanwinter8@gmail.com>
2020-09-28 23:05:42 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -2,10 +2,12 @@
 def run_guessing_game
   number = rand(6) +1
   guess = gets.chomp
-  if guess == number
-    puts "You gueesed the correct number!"
-  elsif guess != number
+  case guess
+  when guess == exit
+    puts "Goodbye!"
+  when guess == number
+    puts "You guessed the correct number!"
+  when guess =! number & guess =! exit
     puts "Sorry! The computer guessed 6."
-    
   end
 end
